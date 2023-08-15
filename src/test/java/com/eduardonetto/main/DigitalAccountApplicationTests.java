3665b60b02da1eceb0e2e3c9ac6690896f190665
Eduardo Netto <dudupnetto.en@gmail.com>
2023-08-15 07:37:59 -0300

    Inital commit

---
+++ src/test/java/com/eduardonetto/main/DigitalAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardonetto.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
