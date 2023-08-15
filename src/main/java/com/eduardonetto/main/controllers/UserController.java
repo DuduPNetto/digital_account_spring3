04e3f87e5bfdff9bb67be5bba2721ac176de1fcb
Eduardo Netto <dudupnetto.en@gmail.com>
2023-08-15 08:37:56 -0300

    User findById

--- src/main/java/com/eduardonetto/main/controllers/UserController.java
+++ src/main/java/com/eduardonetto/main/controllers/UserController.java
@@ -6,10 +6,15 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eduardonetto.main.controllers.dto.InsertDTO;
 import com.eduardonetto.main.controllers.dto.UserDTO;
+import com.eduardonetto.main.entities.User;
 import com.eduardonetto.main.services.UserService;
 
 @RestController
@@ -23,6 +28,12 @@ public class UserController {
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<UserDTO> list = service.findAll().stream().map(x -> service.toDto(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(service.toDto(user));
 	}
 
 }
