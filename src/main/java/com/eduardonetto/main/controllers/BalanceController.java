b745d1c98675c0062b654dc711a8bffbe1d141f2
Eduardo Netto <dudupnetto.en@gmail.com>
2023-08-17 20:37:47 -0300

    BalanceService deposit money

--- src/main/java/com/eduardonetto/main/controllers/BalanceController.java
+++ src/main/java/com/eduardonetto/main/controllers/BalanceController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eduardonetto.main.controllers.dto.BalanceDTO;
+import com.eduardonetto.main.controllers.dto.UserDTO;
 import com.eduardonetto.main.services.BalanceService;
 
 @RestController
@@ -20,6 +23,11 @@ public class BalanceController {
 	@GetMapping("/{id}")
 	public ResponseEntity<BalanceDTO> getBalance(@PathVariable Long id) {
 		return ResponseEntity.ok().body(service.getBalance(id));
+	}
+
+	@PostMapping("/deposit/{id}")
+	public ResponseEntity<UserDTO> deposit(@PathVariable Long id, @RequestBody BalanceDTO balanceDto) {
+		return ResponseEntity.ok().body(service.deposit(id, balanceDto));
 	}
 
 }
--- src/main/java/com/eduardonetto/main/services/BalanceService.java
+++ src/main/java/com/eduardonetto/main/services/BalanceService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.eduardonetto.main.components.JsonFetcher;
 import com.eduardonetto.main.components.data.JsonData;
 import com.eduardonetto.main.controllers.dto.BalanceDTO;
+import com.eduardonetto.main.controllers.dto.UserDTO;
 import com.eduardonetto.main.entities.User;
 import com.eduardonetto.main.services.exceptions.BalanceException;
 
@@ -22,11 +23,29 @@ public class BalanceService {
 		JsonData data = fetcher.fetchJsonData("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6");
 
 		if (!data.getContent().equals("Autorizado")) {
-			throw new BalanceException("You don't have access to this function");
+			throw new BalanceException("You don't have access to this function.");
 		}
 
 		User user = userService.findById(id);
 		return new BalanceDTO(user.getBalance());
+	}
+
+	public UserDTO deposit(Long id, BalanceDTO balanceDto) {
+		JsonData data = fetcher.fetchJsonData("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6");
+
+		if (!data.getContent().equals("Autorizado")) {
+			throw new BalanceException("You don't have access to this function.");
+		}
+
+		if (balanceDto.amount() <= 0) {
+			throw new BalanceException("Amount must be more than 0.");
+		}
+
+		User user = userService.findById(id);
+		user.addBalance(balanceDto.amount());
+		user = userService.save(user);
+		
+		return userService.toDto(user);
 	}
 
 }
